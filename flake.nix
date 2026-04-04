d79cbecb20f104b8eccf2198b9aaf426be6a09f4
weqeqq <ggmodwear@gmail.com>
2026-04-04 19:34:23 +0300

    chore(release): bump version to 0.1.1

--- flake.nix
+++ flake.nix
@@ -50,7 +50,7 @@
             }:
             pkgs.stdenv.mkDerivation {
               pname = "wqcolor";
-              version = "0.1.0";
+              version = "0.1.1";
               inherit src;
 
               strictDeps = true;
